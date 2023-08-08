a9b82e6b0f092b5f3228f0594e7aec667bb171d3
Diogo Silva <diogo.silva.pt14@gmail.com>
2023-08-08 15:45:18 +0100

    Added some verifications when deleting the device class. Implemented /proc entries for debugging as seen in the book

--- modules/02_scull/scull.c
+++ modules/02_scull/scull.c
@@ -38,6 +38,104 @@ module_param(scull_qset, int, S_IRUGO);
 struct scull_dev *scull_devices;	/* allocated in scull_init_module */
 static struct class *scull_class;     /* class used for device file creation */
 
+#ifdef SCULL_DEBUG /* proc implementation for debugging */
+/*
+ * Proc fs: function to read and entry
+ */
+
+static void *scull_seq_start(struct seq_file *s, loff_t *pos)
+{
+    if(*pos >= scull_nr_devs)
+        return NULL; /* No more to read */
+
+    return scull_devices + *pos;
+}
+
+static void *scull_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+    if(++(*pos) >= scull_nr_devs)
+        return NULL;
+
+    return scull_devices + *pos;
+}
+
+static void scull_seq_stop(struct seq_file *s, void *v)
+{
+    /* Nothing to do since no cleanup is necessary */
+}
+
+static int scull_seq_show(struct seq_file *s, void *v)
+{
+    struct scull_dev *dev = (struct scull_dev *) v;
+    struct scull_qset *d;
+    int i;
+
+    if(down_interruptible(&dev->sem))
+        return -ERESTARTSYS;
+
+    seq_printf(s, "\nDevice %i: qset %i, q %i, sz %li\n", (int) (dev - scull_devices),
+                dev->qset, dev->quantum, dev->size);
+
+    for(d = dev->data; d; d = d->next){ /* Scan the list */
+        seq_printf(s, "  item at %p, qset at %p\n", d, d->data);
+        if(d->data && !d->next){ /* Dump only the last item */
+            for(i = 0; i < dev->qset; i++){
+                if(d->data[i])
+                    seq_printf(s, "    % 4i: %8p\n", i, d->data[i]);
+            }
+        }
+    }
+    up(&dev->sem);
+    return 0;
+}
+
+/*
+ * Tie the sequence operators up
+ */
+static struct seq_operations scull_seq_ops = {
+    .start = scull_seq_start,
+    .next = scull_seq_next,
+    .stop = scull_seq_stop,
+    .show = scull_seq_show
+};
+
+/*
+ * Implement the /proc files
+ */
+
+static int scullseq_proc_open(struct inode *inode, struct file *file)
+{
+    return seq_open(file, &scull_seq_ops);
+}
+
+/*
+ * Create a set of file operations for our proc files
+ */
+static struct file_operations scullseq_proc_ops = {
+    .owner      = THIS_MODULE,
+    .open       = scullseq_proc_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = seq_release
+};
+
+/*
+ * Create (and remove) the /proc file(s)
+ */
+
+static void scull_create_proc(void)
+{
+    proc_create("scullseq", 0, NULL, &scullseq_proc_ops);
+}
+
+static void scull_remove_proc(void)
+{
+    /* no problem if it was not registered */
+    remove_proc_entry("scullseq", NULL);
+}
+
+#endif /* SCULL_DEBUG */
+
 /*
  * Empty out the scull device; must be called with the device semaphore held
  */
@@ -349,8 +447,10 @@ void scull_cleanup_module(void)
 		for(i = 0; i < scull_nr_devs; i++){
 			scull_trim(scull_devices + i);
 			cdev_del(&scull_devices[i].cdev);
-            specific_devno = MKDEV(scull_major, scull_minor + i);
-            device_destroy(scull_class, specific_devno);
+            if(scull_class){
+                specific_devno = MKDEV(scull_major, scull_minor + i);
+                device_destroy(scull_class, specific_devno);
+            }
 		}
 		kfree(scull_devices);
 	}
@@ -362,6 +462,10 @@ void scull_cleanup_module(void)
     if(scull_class){
         class_destroy(scull_class);
     }  
+
+#ifdef SCULL_DEBUG
+    scull_remove_proc();
+#endif
 
 	PDEBUG("Scull module cleaned up");
 }
@@ -449,6 +553,10 @@ int scull_init_module(void)
 		sema_init(&scull_devices[i].sem, 1);
 		scull_setup_cdev(&scull_devices[i], i);
 	}
+
+#ifdef SCULL_DEBUG
+    scull_create_proc();
+#endif
 
 	PDEBUG("Scull driver initialized");
 
